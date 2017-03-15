1a8ae0d0c87b7660b4671c3207a690cb33817106
sunnylu1987 <sunnylu1987@163.com>
2017-03-15 22:28:01 +0800

    Done IOMonitor.GetCmdIostat

--- web/models/IOMonitor/index.js
+++ web/models/IOMonitor/index.js
@@ -29,6 +29,14 @@ IOMonitor.prototype.GetCmdIotop = function(options, callback) {
         });
 };
 
+// the output of this command is like below:
+// "Linux 3.13.0-77-generic (ubuntu) \t03/15/17 \t_x86_64_\t(2 CPU)",
+// "",
+// "Device:         rrqm/s   wrqm/s     r/s     w/s    rkB/s    wkB/s avgrq-sz avgqu-sz   await r_await w_await  svctm  %util",
+// "sda               0.03     0.13    0.53    0.09     9.87     3.56    42.81     0.03   42.71    9.87  232.45   6.55   0.41",
+// "",
+// ""
+
 IOMonitor.prototype.GetCmdIostat = function(options, callback) {
 
     var thisMonitor = this;
@@ -45,7 +53,33 @@ IOMonitor.prototype.GetCmdIostat = function(options, callback) {
                 response['command'] = command;
             }
 
-            // TODO:
+            // Done by Ting
+            var line = lines.shift();
+            while( lines.length > 0 && !line.match(/Device:\s+rrqm/)) {
+                line = lines.shift()
+            }
+
+            if (lines.length == 0) {
+                response['error'] = 'Failed to locate a header line beginning with "Device: rrqm..."';
+                callback(response);
+            }
+
+            var headerColumns = line.trim().split(/\s+/);
+            while (lines.length > 0) {
+                line = lines.shift();
+                if (line == "")
+                    continue
+
+                var lineValues = line.trim().split(/\s+/);
+
+                var deviceName = lineValues[0];
+
+                response['data'][deviceName] = {};
+
+                for (var columnIndex = 1; columnIndex < lineValues.length; columnIndex++) {
+                    response['data'][deviceName][headerColumns[columnIndex]] = parseFloat(lineValues[columnIndex]);
+                }
+            }
 
             callback(response);
         })
