88f5133d26d3389ee1fb4942a765a1521fbec2f9
sunnylu1987 <sunnylu1987@163.com>
2017-03-19 14:41:29 +0800

    Implement IOMonitor/GetCmdDf

--- web/models/IOMonitor/GetCmdDf/index.js
+++ web/models/IOMonitor/GetCmdDf/index.js
@@ -13,10 +13,24 @@ GetCmdDfCommander.prototype.parse = function(lines) {
 
     var parsedData = {};
     parsedData['parsed'] = {};
-    parsedData['parsed']['data'] = [];
+    parsedData['parsed']['disks'] = {};
 
     try {
-
+        // Done by Ting
+        for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+            var line = lines[lineIndex].trim();
+            if (!line.startsWith('/dev/')) {
+                continue
+            }
+            var lineValues = line.split(/\s+/);
+            var diskName = lineValues[0].slice(5);
+            parsedData['parsed']['disks'][diskName] = {}
+            parsedData['parsed']['disks'][diskName]['size'] = lineValues[1]
+            parsedData['parsed']['disks'][diskName]['used'] = lineValues[2]
+            parsedData['parsed']['disks'][diskName]['free'] = lineValues[3]
+            parsedData['parsed']['diskTotal'] += parsedData['parsed']['disks'][diskName]['size']
+            parsedData['parsed']['diskUsed'] += parsedData['parsed']['disks'][diskName]['used']
+        }
     } catch( exception ) {
         parsedData['error'] = exception.message;
     }
