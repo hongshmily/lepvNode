6e941e32bb92c72a7bbed88d1cb40795a6fe2047
sunnylu1987 <sunnylu1987@163.com>
2017-03-19 11:10:19 +0800

    Implement MemoryMonitor/GetProcMeminfo

--- web/models/MemoryMonitor/GetProcMeminfo/index.js
+++ web/models/MemoryMonitor/GetProcMeminfo/index.js
@@ -14,7 +14,29 @@ GetProcMeminfoCommander.prototype.parse = function(lines) {
     parsedData['parsed'] = {};
 
     try {
+        // Done by Ting
+        var results = {}
+        for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+            var line = lines[lineIndex];
+            var linePairs = line.trim().split(/:\s+/);
+            if (linePairs.length < 2) {
+                continue;
+            }
+            var lineKey = linePairs[0].trim();
+            var lineValue = linePairs[1].trim();
+            results[lineKey] = lineValue;
+        }
+        parsedData.parsed['name'] = 'memory';
+        parsedData.parsed['unit'] = 'MB';
+        parsedData.parsed['total'] = (parseInt(results['MemTotal']) / 1024).toFixed(1)
+        parsedData.parsed['free'] = (parseInt(results['MemFree']) / 1024).toFixed(1)
+        parsedData.parsed['buffers'] = (parseInt(results['Buffers']) / 1024).toFixed(1)
+        parsedData.parsed['cached'] = (parseInt(results['Cached']) / 1024).toFixed(1)
+        parsedData.parsed['used'] = parsedData.parsed['total'] - parsedData.parsed['free'] - parsedData.parsed['buffers'] - parsedData.parsed['cached']
 
+        var usedRatio = (parsedData.parsed['used'] / parsedData.parsed['total']) * 100
+        usedRatio = usedRatio.toFixed(2)
+        parsedData.parsed["ratio"] = usedRatio
     } catch( exception ) {
         parsedData['error'] = exception.message;
     }
