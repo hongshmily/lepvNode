079f71ce00bce7e6ef00236210c0177abfbf2a28
Mac Xu <mxu@marinsoftware.com>
2017-04-05 12:03:58 +0800

    Introduce Moch and Chai, for testing.

---
+++ web/test/monitors/CpuMonitorTest.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mxu on 4/5/17.
+ */
+
+var chai = require('chai');
+var should = chai.should();
+
+describe('CpuMonitor', function() {
+    it('getProcessorCount');
+    it('GetCmdTop');
+    it('GetAverageLoad');
+    it('GetProcCpuinfo');
+    it('GetCapacity');
+    it('GetCmdMpstat');
+});
